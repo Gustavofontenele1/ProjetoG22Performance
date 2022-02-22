cd0df9801773d1b6390def401203b695f3a6794b
Gustavofontenele1 <gustavofontenele@live.com>
2022-02-21 22:44:58 -0300

    Att responsividade

--- js/main.js
+++ js/main.js
@@ -13,5 +13,10 @@
       });
 
 //Navigation bar effects on scroll 
+window.addEventListener("scroll", function(){
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+  
+})
 
 //responsive navigation menu toggle 
