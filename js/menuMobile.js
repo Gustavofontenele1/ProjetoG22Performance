17d8e484218c5f37a8e62bf1cb6eb11272206456
Gustavofontenele1 <96679768+Gustavofontenele1@users.noreply.github.com>
2022-08-29 01:31:08 -0300

    att

--- js/menuMobile.js
+++ js/menuMobile.js
@@ -1,51 +1,24 @@
-const btn = document.querySelector('.nav-toggle');
-const menu = document.querySelector('#menu-mobile');
+const btn = document.querySelector(".nav-toggle");
+const menu = document.querySelector("#menu-mobile");
 
 function openMenu() {
-  let open = btn.classList.contains('open');
+  let open = btn.classList.contains("open");
 
-    if (!open) {
-      btn.classList.add('open');
-      menu.classList.add('show');
-    } else {
-      btn.classList.remove('open');
-      menu.classList.remove('show');
-    }
-    open = !open;
+  if (!open) {
+    btn.classList.add("open");
+    menu.classList.add("show");
+  } else {
+    btn.classList.remove("open");
+    menu.classList.remove("show");
+  }
+  open = !open;
 }
 btn.addEventListener("click", openMenu);
 
-//Swiper slider
-      var swiper = new Swiper(".bg-slider-thumbs", {
-        loop: true,
-        spaceBetween: 0,
-        slidesPerView: 0,
-      });
-      var swiper2 = new Swiper(".bg-slider", {
-        loop: true,
-        spaceBetween: 0,
-        autoplay: {
-          delay: 5000,
-          disableOnInteraction: false,
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        thumbs: {
-          swiper: swiper,
-        },
-      });
-
-//Navigation bar effects on scroll 
-window.addEventListener("scroll", function(){
+//Navigation bar effects on scroll
+window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
-  
-})
+});
 
-//responsive navigation menu toggle 
+//responsive navigation menu toggle
